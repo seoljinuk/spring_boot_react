6d42731353cb532ff0706135f1a196ccf2bb02de
bluesky <bluesky@example.com>
2025-09-08 20:24:49 +0900

    sprig boot with react app

---
+++ src/main/java/com/coffee/controller/FruitController.java
@@ -0,0 +1,30 @@
+package com.coffee.controller;
+
+
+import com.coffee.entity.Fruit;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FruitController {
+    @GetMapping("/fruit")
+    public Fruit test() {
+        Fruit bean = new Fruit();
+        bean.setPrice(1000);
+        bean.setId("banana");
+        bean.setName("바나나");
+        return bean;
+    }
+
+    @GetMapping("/fruit/list")
+    public List<Fruit> test02() {
+        List<Fruit> fruitList = new ArrayList<Fruit>();
+        fruitList.add(new Fruit("apple", "사과", 1000));
+        fruitList.add(new Fruit("pear", "나주배", 2000));
+        fruitList.add(new Fruit("grape", "포도", 3000));
+        return fruitList;
+    }
+}
