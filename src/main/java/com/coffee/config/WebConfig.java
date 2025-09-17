b2a96f709096723464a1a9c488c6114dfead3431
seoljinuk <96030660+seoljinuk@users.noreply.github.com>
2025-09-17 14:05:37 +0900

    Update WebConfig.java

--- src/main/java/com/coffee/config/WebConfig.java
+++ src/main/java/com/coffee/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations(uploadPath);
     }
 
+    // allowCredentials(true) : 브라우저가 이 서버와 쿠키(혹은 세션 등 인증정보)를 주고받는 것을 허용하겠습니다.
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
